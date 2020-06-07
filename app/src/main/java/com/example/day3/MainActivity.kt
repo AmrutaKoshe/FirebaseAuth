1b2471510c40ef59c0e3cd993086d2da6e65aec3
Amruta Koshe <amruta.koshe@gmail.com>
2020-06-07 10:41:02 +0530

    first commit

--- app/src/main/java/com/example/day3/MainActivity.kt
+++ app/src/main/java/com/example/day3/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.day3
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var providers: List<AuthUI.IdpConfig>
+    val MY_REQUEST_CODE: Int = 7117
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        providers = Arrays.asList<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+
+        showSignInOptions()
 
         button1.setOnClickListener{
             val intent = Intent(this,MainActivity2::class.java)
@@ -24,6 +41,33 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity3::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == MY_REQUEST_CODE)
+        {
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK)
+            {
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this,""+user!!.email,Toast.LENGTH_SHORT).show()
+
+                button1.isEnabled = true
+                button2.isEnabled = true
+            }
+            else
+            {
+                Toast.makeText(this,""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.MyTheme)
+            .build(),MY_REQUEST_CODE)
     }
 
 }
